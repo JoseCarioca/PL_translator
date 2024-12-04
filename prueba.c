3cd94771080fae795dfe33b495f0613aef8fd31b
JoseCarioca <65904569+JoseCarioca@users.noreply.github.com>
2024-12-04 09:52:56 +0100

    PLiter1
    Added first iteration of code revision
    all changes are meant to improve assembly translation, reduce redundancy and generalize code use

--- prueba.c
+++ prueba.c
@@ -1,11 +1,8 @@
 
+int G[3];
 int main ()
 {
-    int A[2][3];
-
-    A[0][0] = 5;
-
-    int Temp = A[0][1] + 5 - 7; //si var empieza por 't' no la reconoce jaja
-
+    int a,b,c;
+    a = b = c = 5;
     return 0;
 }
